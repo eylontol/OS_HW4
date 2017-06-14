09d8219114f9c113e9ff9ec5c789fd62828b9e37
Eylon <eylontol@gmail.com>
2017-06-14 14:03:33 +0300

    I <3 Lables

--- intercept.c
+++ intercept.c
@@ -1,23 +1,34 @@
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-// TODO: add more #include statements as necessary
+#include <linux/utsname.h>
+#include <unistd.h>
 
 MODULE_LICENSE("GPL");
 
-// TODO: add command-line arguments
+extern asmlinkage ssize_t sys_read(unsigned int fd, char * buf, size_t count);
+
 void** sys_call_table = NULL;
 
 // TODO: import original syscall and write new syscall
 
 void find_sys_call_table(int scan_range) {
-   // TODO: complete the function
+    int *curr = (int *)(&system_utsname), i;
+    for (i = 0; i < scan_range; ++i)
+        if (*(curr++) == (int)sys_read) {
+            sys_call_table = ((void **)(curr - 3 /* __NR_read */));
+            break;
+        }
 }
 
 int init_module(void) {
-   // TODO: complete the function
+    printk("init_module\n");
+    find_sys_call_table(136);
+    return 0;
 }
 
 void cleanup_module(void) {
-   // TODO: complete the function
+    printk("cleanup_module\n");
 }
-
