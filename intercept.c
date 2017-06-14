c670efcd9cef99dc94bd314d6ded75eb6db0ce56
yuvalshilo <yuvalman@gmail.com>
2017-06-14 14:51:44 +0300

    and

--- intercept.c
+++ intercept.c
@@ -6,17 +6,21 @@
 
 MODULE_LICENSE("GPL");
 
+
 void** sys_call_table = NULL;
 
 // TODO: import original syscall and write new syscall
 
-void find_sys_call_table(int scan_range) {
-    int *curr = (int *)(&system_utsname), i;
-    for (i = 0; i < scan_range; ++i)
-        if (*(curr++) == (int)sys_read) {
-            sys_call_table = ((void **)(curr - 3 /* __NR_read */));
-            break;
-        }
+
+void find_sys_call_table(int scan_range)
+{
+    void* i = &system_utsname;
+    for (;*((unsigned long *)i) != (unsigned long)sys_read && scan_range > 0; i++, scan_range--);
+    if (*((unsigned long *)i) == (unsigned long)sys_read)
+    {
+        i-=__NR_read;
+        sys_call_table = i;
+    }
 }
 
 int init_module(void) {
@@ -24,9 +28,12 @@ int init_module(void) {
     find_sys_call_table(136);
     if (sys_call_table) printk("sys_call_table address = 0x%08x\n", (int)sys_call_table);
     else printk("Couldn't find syscall_table address\n");
+
     return 0;
 }
 
 void cleanup_module(void) {
-    printk("cleanup_module\n");
+
+   // TODO: complete the function
+    printk("done\n");
 }
